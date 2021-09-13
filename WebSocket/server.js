3d461b9b5440c4172dd9dcb84e7aa8fa289ecb6c
BenediktMehl <b.mehl14@outlook.de>
2021-09-13 17:11:32 +0200

    server runs now on new version

--- WebSocket/server.js
+++ WebSocket/server.js
@@ -11,6 +11,7 @@ const wss = new WebSocket.Server({ server });
 appClients = {}; //Room-ID -> app connected to the server
 webClients = {}; //Room-ID -> web client that controls an avatar 
 jitsiURLs = {}; //Room-ID -> jitsi url 
+controlElements = {}; //Room-ID -> control elements
 IDs = [];	//Room-ID
 
 
@@ -24,9 +25,11 @@ wss.on('connection', function(ws, request, client) {
             IDs.push(id);
             
 			//remember jitsi URL
-            jitsiURLs[id] = message.split(':')[1]; 
+            messageArguments = message.split(':');
+            jitsiURLs[id] = messageArguments[1];
+            controlElements[id] = messageArguments[2];
 			console.log(jitsiURLs[id]);
-            
+
 			//create entry for app
             appClients[id] = ws;
             appClients[id].myid = id;
@@ -34,9 +37,16 @@ wss.on('connection', function(ws, request, client) {
 			//send room-id to app
             appClients[id].send('id:' + id);
             
+			appClients[id].on('message', function(m) {
+                //if web is still connected -> send (probably stall)
+				if (webClients[this.myid] != null) {
+					webClients[this.myid].send(m);  
+				}
+            });
+			
             appClients[id].on('close', function() {
-                removeSession(this.myid);//session is just removed, when there is no app client -> meaning session is just removed when there is no app and web client after this method
-                delete appClients[id];
+				delete appClients[id];
+                removeSession(this.myid);
             });
 			
 		//communicating with web
@@ -49,7 +59,7 @@ wss.on('connection', function(ws, request, client) {
                 webClients[id].myid = id;
                 
 				//send jitsi-url to web client
-				webClients[id].send(jitsiURLs[id]);
+				webClients[id].send(jitsiURLs[id] + ":" + controlElements[id]);
                 
                 webClients[id].on('message', function(m) {
 					//when app is still connected -> send
@@ -63,16 +73,17 @@ wss.on('connection', function(ws, request, client) {
                 });
 				
                 webClients[id].on('close', function() {
-                    removeSession(this.myid); //session is just removed, when there is no app client -> meaning session is just removed when there is no app and web client after this method
-                    delete webClients[this.myid];
+					delete webClients[this.myid];
+                    removeSession(this.myid); //session is just removed when there is no app and web client
                 });
 				
 			//when there is already one web client connected 
             } else if (appClients[id] != null) {
-                ws.send(jitsiURLs[id] + "/");
+                ws.send("FULL:" + jitsiURLs[id] + ":" + id);
             //when room-id doesn't exist 
 			}else{
-				//TODO: what to do if id is invalid
+				ws.send("INVALID: Id "+id+" is invalid!");
+				console.log("INVALID: Id "+id+" is invalid!");
 			}	
         }
         if (!message.includes(";")) console.log(countProperties(appClients) + ", " + countProperties(webClients))
@@ -81,13 +92,24 @@ wss.on('connection', function(ws, request, client) {
 
 //removes room-id from list, if there is no app or no web client
 function removeSession(id) {
-    if (appClients[id] == null || webClients[id] == null) {
+    if (appClients[id] == null && webClients[id] == null) {
+		console.log("session "+id+" removed");
         delete jitsiURLs[id];
         var index = IDs.indexOf(id);
         if (index > -1) {
             IDs.splice(index, 1);
         }
-    }
+    }else if(appClients[id] != null && webClients[id] == null){ //web disconnected before app
+		console.log("web "+id+" disconnected");
+		appClients[id].send("DISCONNECT");
+	}else if(webClients[id] != null && appClients[id] == null ){//app disconnected before web
+		console.log("app "+id+" disconnected");
+		webClients[id].send("DISCONNECT");
+		delete webClients[id]; //delete web clients entry because if app disconnects web can't do anything and is redirected to start page
+		removeSession(id);
+	}else{
+		console.log("This should not happen!");
+	}
     console.log(countProperties(appClients) + ", " + countProperties(webClients));
 }
 
